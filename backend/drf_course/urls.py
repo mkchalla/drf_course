d9a8863f558cc7c217b5328822bf2983f247cb2f
mchalla <mchalla.dev@live.com>
2023-12-03 00:33:21 +0000

    updated for module_5

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
 from core import views as core_views
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 
@@ -25,4 +26,5 @@ urlpatterns = router.urls
 urlpatterns += [
     path('admin/', admin.site.urls),
     path('contact/', core_views.ContactAPIView.as_view()),
+    path('api-token-auth/', obtain_auth_token), #gives us access to token auth
 ]
